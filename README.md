bf35e34082702561f6a6950c3cd3ad1d8a8d25ac
gallianoms <ggallianoms@gmail.com>
2024-02-21 09:03:17 +0100

    Change logic AppointmentControllerUnitTest, EntityControllerUnitTest.java, EntityUnitTest. Add Dockerfiles (2) and README.md

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# java-template
+# AccWe Hospital
+
+Este proyecto tiene como objetivo renovar el sistema de control de citas en el hospital AccWe, que ha experimentado una serie de ataques informáticos recientes. El sistema anterior se ha revisado y limpiado para evitar posibles errores, priorizando la actualización de librerías y tecnologías.
+
+## Requisitos para Utilizar el Proyecto
+
+- Java 8 o superior
+- Maven
+- Docker
+
+## Crear Imágenes Docker
+
+- docker build -t accwe-hospital-mysql -f Dockerfile.mysql .
+
+- docker build -t accwe-hospital-api -f Dockerfile.maven .
+
+## Crear contenedores Docker
+
+- docker run -d -p 3306:3306 --name accwe-mysql-container accwe-hospital-mysql:latest
+
+- docker run -d -p 8080:8080 --name accwe-api-container accwe-hospital-api:latest
+
+## UML diagram
+
+![Hospital AccWe](UML-accwe-hospital.png)
--- src/test/java/com/example/demo/EntityControllerUnitTest.java
+++ src/test/java/com/example/demo/EntityControllerUnitTest.java
@@ -1,7 +1,9 @@
 
 package com.example.demo;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 
@@ -41,61 +43,297 @@ import com.fasterxml.jackson.databind.ObjectMapper;
  */
 
 @WebMvcTest(DoctorController.class)
-class DoctorControllerUnitTest{
+class DoctorControllerUnitTest {
 
     @MockBean
     private DoctorRepository doctorRepository;
 
-    @Autowired 
+    @Autowired
     private MockMvc mockMvc;
 
     @Autowired
     private ObjectMapper objectMapper;
 
     @Test
-    void this_is_a_test(){
-        // DELETE ME
-        assertThat(true).isEqualTo(false);
+    void givenValidDoctors_whenGetAllDoctors_thenReturnsListOfDoctorsWithStatusCode200() throws Exception {
+        List<Doctor> doctors = new ArrayList<>();
+        doctors.add(new Doctor("John", "Doe", 30, "john.doe@hospital.com"));
+        when(doctorRepository.findAll()).thenReturn(doctors);
+
+        mockMvc.perform(get("/api/doctors"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].id").isNumber())
+                .andExpect(jsonPath("$[0].firstName").value("John"))
+                .andExpect(jsonPath("$[0].lastName").value("Doe"))
+                .andExpect(jsonPath("$[0].age").value(30))
+                .andExpect(jsonPath("$[0].email").value("john.doe@hospital.com"));
     }
+
+    @Test
+    void createDoctor_ReturnsCreatedDoctor() throws Exception {
+
+        Doctor doctorToCreate = new Doctor("John", "Doe", 30, "john.doe@hospital.com");
+        when(doctorRepository.save(eq(doctorToCreate))).thenAnswer(invocation -> invocation.getArgument(0));
+
+        mockMvc.perform(post("/api/doctor")
+                        .contentType("application/json")
+                        .content(objectMapper.writeValueAsString(doctorToCreate)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.firstName").value("John"))
+                .andExpect(jsonPath("$.lastName").value("Doe"))
+                .andExpect(jsonPath("$.age").value(30))
+                .andExpect(jsonPath("$.email").value("john.doe@hospital.com"))
+                .andExpect(jsonPath("$.firstName").isString())
+                .andExpect(jsonPath("$.lastName").isString())
+                .andExpect(jsonPath("$.age").isNumber())
+                .andExpect(jsonPath("$.firstName").isString());
+    }
+
+    @Test
+    void givenNoValidDoctorId_whenDeleteDoctor_thenReturnsNoContentWithStatusCode404() throws Exception {
+        long doctorId = 1L;
+
+        when(doctorRepository.findById(doctorId)).thenReturn(Optional.empty());
+
+        mockMvc.perform(delete("/api/doctors/{id}", doctorId))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void givenValidDoctorId_whenDeleteDoctor_thenReturnsOkWithStatusCode200() throws Exception {
+
+        long doctorId = 1L;
+
+        when(doctorRepository.findById(doctorId)).thenReturn(Optional.of(new Doctor("John", "Doe",
+                30, "john.doe@hospital.com")));
+
+        mockMvc.perform(delete("/api/doctors/{id}", doctorId))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void givenValidDoctorId_whenGetDoctorById_theReturnsOkWithDoctor() throws Exception {
+        long doctorId = 1L;
+
+        when(doctorRepository.findById(doctorId)).thenReturn(Optional.of(new Doctor("John", "Doe",
+                30, "john.doe@hospital.com")));
+
+        mockMvc.perform(get("/api/doctors/{id}", doctorId))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.firstName").value("John"))
+                .andExpect(jsonPath("$.lastName").value("Doe"))
+                .andExpect(jsonPath("$.age").value(30))
+                .andExpect(jsonPath("$.email").value("john.doe@hospital.com"));
+    }
+
+    @Test
+    void givenNoDoctors_whenGetAllDoctors_thenReturnsEmptyListWithStatusCode204() throws Exception {
+        when(doctorRepository.findAll()).thenReturn(Collections.emptyList());
+
+        mockMvc.perform(get("/api/doctors"))
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+    void givenInvalidDoctorId_whenGetDoctorById_thenReturnsNotFoundWithStatusCode404() throws Exception {
+        long invalidDoctorId = -1L;
+
+        when(doctorRepository.findById(invalidDoctorId)).thenReturn(Optional.empty());
+
+        mockMvc.perform(get("/api/doctors/{id}", invalidDoctorId))
+                .andExpect(status().isNotFound());
+    }
+
 }
 
 
 @WebMvcTest(PatientController.class)
-class PatientControllerUnitTest{
+class PatientControllerUnitTest {
 
     @MockBean
     private PatientRepository patientRepository;
 
-    @Autowired 
+    @Autowired
     private MockMvc mockMvc;
 
     @Autowired
     private ObjectMapper objectMapper;
 
     @Test
-    void this_is_a_test(){
-        // DELETE ME
-        assertThat(true).isEqualTo(false);
+    void givenValidPatients_whenGetAllPatients_thenReturnsListOfPatientsWithStatusCode200() throws Exception {
+        List<Patient> patients = new ArrayList<>();
+        patients.add(new Patient("Jane", "Smith", 20, "jane.smith@hospital.com"));
+        when(patientRepository.findAll()).thenReturn(patients);
+
+        mockMvc.perform(get("/api/patients"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].id").isNumber())
+                .andExpect(jsonPath("$[0].firstName").value("Jane"))
+                .andExpect(jsonPath("$[0].lastName").value("Smith"))
+                .andExpect(jsonPath("$[0].age").value(20))
+                .andExpect(jsonPath("$[0].email").value("jane.smith@hospital.com"));
+    }
+
+    @Test
+    void createPatient_ReturnsCreatedPatient() throws Exception {
+
+        Patient patientToCreate = new Patient("Jane", "Smith", 20, "jane.smith@hospital.com");
+        when(patientRepository.save(eq(patientToCreate))).thenAnswer(invocation -> invocation.getArgument(0));
+
+        mockMvc.perform(post("/api/patient")
+                        .contentType("application/json")
+                        .content(objectMapper.writeValueAsString(patientToCreate)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.firstName").value("Jane"))
+                .andExpect(jsonPath("$.lastName").value("Smith"))
+                .andExpect(jsonPath("$.age").value(20))
+                .andExpect(jsonPath("$.email").value("jane.smith@hospital.com"))
+                .andExpect(jsonPath("$.firstName").isString())
+                .andExpect(jsonPath("$.lastName").isString())
+                .andExpect(jsonPath("$.age").isNumber())
+                .andExpect(jsonPath("$.firstName").isString());
+    }
+
+    @Test
+    void givenNoValidPatientId_whenDeletePatient_thenReturnsNoContentWithStatusCode404() throws Exception {
+        long patientId = 1L;
+
+        when(patientRepository.findById(patientId)).thenReturn(Optional.empty());
+
+        mockMvc.perform(delete("/api/patients/{id}", patientId))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void givenValidPatientId_whenDeletePatient_thenReturnsOkWithStatusCode200() throws Exception {
+
+        long patientId = 1L;
+
+        when(patientRepository.findById(patientId)).thenReturn(Optional.of(new Patient(
+                "Jane", "Smith", 20, "jane.smith@hospital.com")));
+
+        mockMvc.perform(delete("/api/patients/{id}", patientId))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void givenValidPatientId_whenGetPatientById_theReturnsOkWithPatient() throws Exception {
+        long patientId = 1L;
+
+        when(patientRepository.findById(patientId)).thenReturn(Optional.of(new Patient("Jane", "Smith",
+                20, "jane.smith@hospital.com")));
+
+        mockMvc.perform(get("/api/patients/{id}", patientId))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.firstName").value("Jane"))
+                .andExpect(jsonPath("$.lastName").value("Smith"))
+                .andExpect(jsonPath("$.age").value(20))
+                .andExpect(jsonPath("$.email").value("jane.smith@hospital.com"));
+    }
+
+    @Test
+    void givenNoPatients_whenGetAllPatients_thenReturnsEmptyListWithStatusCode204() throws Exception {
+        when(patientRepository.findAll()).thenReturn(Collections.emptyList());
+
+        mockMvc.perform(get("/api/patients"))
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+    void givenInvalidPatientId_whenGetPatientById_thenReturnsNotFoundWithStatusCode404() throws Exception {
+        long invalidPatientId = -1L;
+
+        when(patientRepository.findById(invalidPatientId)).thenReturn(Optional.empty());
+
+        mockMvc.perform(get("/api/patients/{id}", invalidPatientId))
+                .andExpect(status().isNotFound());
     }
 
 }
 
 @WebMvcTest(RoomController.class)
-class RoomControllerUnitTest{
+class RoomControllerUnitTest {
 
     @MockBean
     private RoomRepository roomRepository;
 
-    @Autowired 
+    @Autowired
     private MockMvc mockMvc;
 
     @Autowired
     private ObjectMapper objectMapper;
 
     @Test
-    void this_is_a_test(){
-        // DELETE ME
-        assertThat(true).isEqualTo(false);
+    void givenValidRoom_whenGetAllRooms_thenReturnsListOfRoomsWithStatusCode200() throws Exception {
+        List<Room> rooms = new ArrayList<>();
+
+        rooms.add(new Room("psychiatry"));
+        when(roomRepository.findAll()).thenReturn(rooms);
+
+        mockMvc.perform(get("/api/rooms"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].roomName").isString())
+                .andExpect(jsonPath("$[0].roomName").value("psychiatry"));
+    }
+
+    @Test
+    void createRoom_ReturnsCreatedRoom() throws Exception {
+
+        Room roomToCreate = new Room("psychiatry");
+        when(roomRepository.save(eq(roomToCreate))).thenAnswer(invocation -> invocation.getArgument(0));
+
+        mockMvc.perform(post("/api/room")
+                        .contentType("application/json")
+                        .content(objectMapper.writeValueAsString(roomToCreate)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.roomName").value("psychiatry"))
+                .andExpect(jsonPath("$.roomName").isString());
+    }
+
+    @Test
+    void givenNoValidRoomName_whenDeleteRoom_thenReturnsNoContentWithStatusCode404() throws Exception {
+        String roomName = "orthopedic";
+
+        when(roomRepository.findByRoomName(roomName)).thenReturn(Optional.empty());
+
+        mockMvc.perform(delete("/api/rooms/{roomName}", roomName))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void givenValidRoomName_whenDeleteDoctor_thenReturnsOkWithStatusCode200() throws Exception {
+
+        String roomName = "orthopedic";
+
+        when(roomRepository.findByRoomName(roomName)).thenReturn(Optional.of(new Room("psychiatry")));
+
+        mockMvc.perform(delete("/api/rooms/{roomName}", roomName))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void givenValidRoomName_whenGetRoomName_theReturnsOkWithRoom() throws Exception {
+        String roomName = "dermatology";
+
+        when(roomRepository.findByRoomName(roomName)).thenReturn(Optional.of(new Room("dermatology")));
+
+        mockMvc.perform(get("/api/rooms/{roomName}", roomName))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.roomName").value("dermatology"));
+    }
+
+    @Test
+    void givenInvalidRoomName_whenGetRoomName_thenReturnsNotFoundWithStatusCode404() throws Exception {
+        String invalidRoomName = "as345asd8";
+
+        when(roomRepository.findByRoomName(invalidRoomName)).thenReturn(Optional.empty());
+
+        mockMvc.perform(get("/api/rooms/{roomName}", invalidRoomName))
+                .andExpect(status().isNotFound());
     }
 
 }
+
